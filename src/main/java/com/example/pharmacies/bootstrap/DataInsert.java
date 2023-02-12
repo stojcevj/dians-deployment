7df893a7c947e8f895be7fdc313a3c353077b953
Jovan Stojcev <stojcevjovan@hotmail.com>
2023-02-12 22:06:46 +0100

    Add goto on every location

--- src/main/java/com/example/pharmacies/bootstrap/DataInsert.java
+++ src/main/java/com/example/pharmacies/bootstrap/DataInsert.java
@@ -19,11 +19,9 @@ public class DataInsert {
 
     @PostConstruct
     public void init() throws FileNotFoundException {
-      //  BufferedReader br = new BufferedReader(new FileReader(""));
+        /*BufferedReader br = new BufferedReader(new FileReader(""));
 
-//test;
-      // String name, String location, String workingTime, Double lon, Double lat, String phoneNumber, String city, String website
-       /* br.lines().forEach(i->{
+        br.lines().forEach(i->{
                    String [] line = i.split("\\|");
                    pharmacyRepository.save(new Pharmacy(line[2], line[3], line[7],
                            Double.parseDouble(line[0]), Double.parseDouble(line[1]), line[5], line[4], line[6]));
